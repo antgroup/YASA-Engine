d6140af0ea73abb024206af4c49be2f6d7de88b8
王雅仪 <wangyayi.wyy@antgroup.com>
2025-11-05 12:10:51 +0800

    reformat

--- src/checker/taint/go/restful-entrypoint-collect-checker.ts
+++ src/checker/taint/go/restful-entrypoint-collect-checker.ts
@@ -1,42 +1,76 @@
-const config = require("../../../config");
+const config = require('../../../config')
 
-const RouteRegistryProperty = ["Filter", "To", "If"]
-const RouteRegistryObject = ["github.com/emicklei/go-restful/v3.WebService<instance>", "github.com/emicklei/go-restful.WebService<instance>"]
-const IntroduceTaint = require("../common-kit/source-util");
-const Checker = require("../../common/checker");
-const { completeEntryPoint } = require("./entry-points-util");
+const RouteRegistryProperty = ['Filter', 'To', 'If']
+const RouteRegistryObject = [
+    'github.com/emicklei/go-restful/v3.WebService<instance>',
+    'github.com/emicklei/go-restful.WebService<instance>',
+]
+const IntroduceTaint = require('../common-kit/source-util')
+const Checker = require('../../common/checker')
+const { completeEntryPoint } = require('./entry-points-util')
 
-const processedRouteRegistry = new Set();
+const processedRouteRegistry = new Set()
 
+/**
+ *
+ */
 class RestfulEntrypointCollectChecker extends Checker {
+    /**
+     *
+     * @param resultManager
+     */
     constructor(resultManager: any) {
-        super(resultManager, "go-restful-entryPoints-collect-checker");
+        super(resultManager, 'go-restful-entryPoints-collect-checker')
     }
 
+    /**
+     *
+     * @param analyzer
+     * @param scope
+     * @param node
+     * @param state
+     * @param info
+     */
     triggerAtFunctionCallBefore(analyzer: any, scope: any, node: any, state: any, info: any) {
-        const { fclos, argvalues } = info;
+        const { fclos, argvalues } = info
 
-        this.collectRouteRegistry(node, fclos, argvalues, scope, info);
+        this.collectRouteRegistry(node, fclos, argvalues, scope, info)
     }
 
+    /**
+     *
+     * @param analyzer
+     * @param scope
+     * @param node
+     * @param state
+     * @param info
+     */
     triggerAtSymbolInterpretOfEntryPointAfter(analyzer: any, scope: any, node: any, state: any, info: any) {
-        if (info?.entryPoint.functionName === 'main') processedRouteRegistry.clear();
+        if (info?.entryPoint.functionName === 'main') processedRouteRegistry.clear()
     }
 
+    /**
+     *
+     * @param callExpNode
+     * @param calleeFClos
+     * @param argValues
+     * @param scope
+     * @param info
+     */
     collectRouteRegistry(callExpNode: any, calleeFClos: any, argValues: any, scope: any, info: any) {
-        const { analyzer, state } = info;
-        if (config.entryPointMode === 'ONLY_CUSTOM') return;
-        if (!(calleeFClos && calleeFClos.object && calleeFClos.property)) return;
-        const { object, property } = calleeFClos;
-        if (!object._qid || !property.name) return;
-        const objectQid = object._qid;
-        const propertyName = property.name;
+        const { analyzer, state } = info
+        if (config.entryPointMode === 'ONLY_CUSTOM') return
+        if (!(calleeFClos && calleeFClos.object && calleeFClos.property)) return
+        const { object, property } = calleeFClos
+        if (!object._qid || !property.name) return
+        const objectQid = object._qid
+        const propertyName = property.name
         if (
             RouteRegistryObject.some((prefix) => objectQid.startsWith(prefix)) &&
             RouteRegistryProperty.includes(propertyName)
         ) {
-            if (argValues.length < 1) return;
-            const arg0 = argValues[0];
+            if (argValues.length < 1) return
+            const arg0 = argValues[0]
 
             if (arg0?.vtype === 'fclos' && arg0?.ast.loc) {
                 const hash = JSON.stringify(arg0.ast.loc)
@@ -51,4 +85,4 @@ class RestfulEntrypointCollectChecker extends Checker {
     }
 }
 
-module.exports = RestfulEntrypointCollectChecker;
+module.exports = RestfulEntrypointCollectChecker
